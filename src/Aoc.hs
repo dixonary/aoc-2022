549cd5b19cd210e5be41e4fc5fee6108c98e02d8
Alex Dixon <alex@dixonary.co.uk>
2022-12-01 11:44:20 +0000

    Day 1

--- src/Aoc.hs
+++ src/Aoc.hs
@@ -1,4 +1,20 @@
 module Aoc where
 
-day01a :: Integer -> String
-day01a = show
+import Witch ( From(..) )
+import Data.List.Split ( splitOn )
+import Data.List
+
+--------------------------------------------------------------------------------
+-- DAY 01
+
+newtype Day01 = Day01 [Integer]
+instance From String Day01 where 
+  from = Day01 . reverse . sort 
+       . map (sum . map read . splitOn "\n") 
+       . splitOn "\n\n"
+
+day01a :: Day01 -> Integer
+day01a (Day01 cs) = head cs
+
+day01b :: Day01 -> Integer
+day01b (Day01 cs) = sum $ take 3 cs
